624f3eb66b699223ee8e3b7844c00004327db76d
naylson <naylsonrj@gmail.com>
2022-10-24 20:28:20 -0300

    monstra img dos pokemons

--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -1,13 +1,59 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <div>
-    <h1>{{num}} - {{name | uppercase }}</h1>
-    <small>{{url}}</small>
+  <div id="pokemon">   
+
+
+  <div class="card">
+    <div class="card-image ">
+      <figure class="media-center">
+        <img :src="pokemon.other"  alt="Pokedex API">
+      </figure>
+    </div>
+    <div class="card-content">
+      <div class="media">
+        <div class="media-left">
+
+          
+          <figure class="image is-64x64">
+            <img :src="pokemon.back" class="is-rounded"  alt="Pokedex API">
+          </figure>
+        </div>
+        <div class="media-content">
+              <p class="title is-4">{{num}} - {{name | uppercase }}</p>
+          <p class="subtitle is-6">{{ pokemon.type }}</p>
+        </div>
+      </div>
+
+ 
+  </div>
+</div>
+
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
+  created: function(){
+    axios.get(this.url).then(res => {
+      this.pokemon.type = res.data.types[0].type.name;
+      this.pokemon.front = res.data.sprites.front_default;
+      this.pokemon.back = res.data.sprites.back_default;
+      this.pokemon.other = res.data.sprites.other.dream_world.front_default;
+    }).catch(error => {
+      console.log(error);
+    });
+  }, 
+  data() {
+    return {
+      pokemon: {
+        type: '',
+        front: '',
+        back: ''
+      }
+    }
+  },
     props: {
     num: Number,
     name: String,
@@ -26,4 +72,12 @@ export default {
 </script>
 
 <style>
+  #pokemon {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 40px;
+  }
 </style>
