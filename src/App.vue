d1f0abbd7221e23147b17c4fe620cb8e27d96433
naylson <naylsonrj@gmail.com>
2022-10-24 19:23:51 -0300

    Exibe a lista de pokemons

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,39 @@
 <template>
   <div id="app">
-    
+    <div v-for="(poke, index) in pokemons" :key="index" >
+      <Pokemon :name="poke.name" :url="poke.url" :num="index+1"/>
+
+    </div>
   </div>
 </template>
 
 <script>
-// import axios from 'axios';
+import axios from 'axios';
+import Pokemon from './components/Pokemon';
+
+
 export default {
   name: 'App',
+  data() {
+    return {
+      pokemons: [],
+    }
+  },
+  created: function(){
+    axios.get('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0')
+    .then(res => {
+      console.log("lista pokemons");
+      this.pokemons = res.data.results;
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  },
+  components: {
+    Pokemon
+  
   }
+}
 </script>
 
 <style>
---
+++ src/components/Pokemon.vue
@@ -0,0 +1,29 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div>
+    <h1>{{num}} - {{name | uppercase }}</h1>
+    <small>{{url}}</small>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+    num: Number,
+    name: String,
+    url: String,
+    
+  }, 
+  filters: {
+    uppercase: function (value) {
+      if (!value) return ''
+      value = value.toString()
+      return value.toUpperCase()
+    }
+
+  }
+}
+</script>
+
+<style>
+</style>
